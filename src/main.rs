b4bee9942152e37cb84b988ab77a0b9f71b2e452
xfnw <xfnw@riseup.net>
2022-11-09 22:41:33 -0500

    move SocketAddr to a function

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,19 @@ use std::net::SocketAddr;
 pub mod handlers;
 pub mod templates;
 
+fn get_listen() -> SocketAddr {
+    // FIXME: allow customizing this via args or environment variable
+    SocketAddr::from(([127, 0, 0, 1], 8326))
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(handlers::root))
+        .route("/style.css", get(handlers::css))
         .fallback(handlers::handler_404.into_service());
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8326));
+    let addr = get_listen();
     eprintln!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())

7f8532da1d4f30937109fb577e3f178ef30e4e46
xfnw <xfnw@riseup.net>
2022-11-11 21:52:48 -0500

    quote pages

--- src/handlers.rs
+++ src/handlers.rs
@@ -53,11 +53,58 @@ async fn read_file(filename: String) -> Result<Vec<u8>, Box<dyn Error>> {
     }
 }
 
+fn make_quote_page(quoteid: u32, content: String) -> Html<String> {
+    // TODO: set this somewhere else lol
+    let min = 0;
+    let max = 69;
+
+    let mut previous = quoteid;
+    let mut next = quoteid;
+    if quoteid > min {
+        previous = quoteid - 1;
+    }
+    if quoteid < max {
+        next = quoteid + 1;
+    }
+    let previous = previous; // un-mut
+    let next = next;
+
+    Html(
+        templates::BaseHtml {
+            title: format!("quote #{}", quoteid),
+            content: templates::QuoteHtml {
+                first: min,
+                last: max,
+                previous: previous,
+                next: next,
+                quote: content,
+            }
+            .to_string(),
+        }
+        .to_string(),
+    )
+}
+
 pub async fn view_quote(param: Path<String>) -> impl IntoResponse {
     //(StatusCode::OK,Html("meow".to_string()))
-    match param.parse::<u16>() {
+    match param.parse::<u32>() {
         Ok(quoteid) => match read_file(format!("quotes/{}.txt", quoteid)).await {
-            Ok(content) => (StatusCode::OK,Html("meow".to_string())),
+            Ok(content) => match String::from_utf8(content) {
+                Ok(ucontent) => (StatusCode::OK, make_quote_page(quoteid, ucontent)),
+                Err(_) => (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Html(
+                        templates::BaseHtml {
+                            title: "500 internal server error".to_string(),
+                            content: format!(
+                                "there was an error converting quote {} to utf8",
+                                quoteid
+                            ),
+                        }
+                        .to_string(),
+                    ),
+                ),
+            },
             Err(_) => tuple_404(),
         },
         Err(_) => tuple_404(),

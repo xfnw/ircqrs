6a37656c3a5cd6a012f832cc57fd807063514e0c
xfnw <xfnw@riseup.net>
2023-03-02 16:33:27 -0500

    move axum::Router creation into separate function
    allows for embedding ircqrs into another application with
    axum::Router::nest

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,23 @@ fn get_listen() -> SocketAddr {
     SocketAddr::from(([127, 0, 0, 1], 8326))
 }
 
-#[tokio::main]
-async fn main() {
-    // force lazy_static to initalize
-    eprintln!("loaded {} quotes", (*handlers::QUOTEENTRIES).len());
-
-    let app = Router::new()
+pub fn create_router() -> Router {
+    Router::new()
         .route("/", get(handlers::root))
         .route("/health", get(handlers::healthping))
         .route("/random", get(handlers::random))
         .route("/style.css", get(handlers::css))
         .route("/robots.txt", get(handlers::robots))
         .route("/:quote", get(handlers::view_quote))
-        .fallback(handlers::handler_404);
+        .fallback(handlers::handler_404)
+}
+
+#[tokio::main]
+async fn main() {
+    // force lazy_static to initalize
+    eprintln!("loaded {} quotes", (*handlers::QUOTEENTRIES).len());
+
+    let app = create_router();
 
     let addr = get_listen();
     eprintln!("listening on {}", addr);
